9df0d27c1bb3f67af55fcc573e8d33e533b1956c
Daolin Sheng <15900859362@163.com>
2021-11-10 15:47:13 +0000

    init commits springboot

---
+++ springboot/springboot-demo/src/main/java/com/sdl/find/boot/FindSBootApplication.java
@@ -0,0 +1,13 @@
+package com.sdl.find.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindSBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindSBootApplication.class, args);
+	}
+}
+
