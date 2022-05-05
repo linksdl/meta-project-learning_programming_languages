78f40611a6d903d54ad9ce84447d6db47c4260e2
Daolin Sheng <15900859362@163.com>
2022-05-06 00:07:58 +0800

    C++基础语法 for C++ basic

---
+++ c++ nowcoder/00-基础语法.cpp
@@ -0,0 +1,23 @@
+//
+// Created by DAOLIN SHENG on 2022/5/5.
+//
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // 简单输出
+    cout << "Hello Nowcoder!"; // Hello Nowcoder!
+
+    cout << "     **     " << endl; // 小飞机
+    cout << "     **     " << endl;
+    cout << "************" << endl;
+    cout << "************" << endl;
+    cout << "    *  *    " << endl;
+    cout << "    *  *    " << endl;
+
+
+    return 0;
+}
